f50923aa9a8125ccd1de59eec505dee581e37ce4
王健 <wangjian@igetcool.com>
2021-08-27 15:30:28 +0800

    增加约束布局链式布局demo

---
+++ app/src/main/java/com/wj/uidemo/ConstraintLayoutChainActivity.kt
@@ -0,0 +1,17 @@
+package com.wj.uidemo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.wj.qmuidemo.R
+
+/**
+ * Des 约束布局链
+ * @author WangJian on 2021/8/27 15
+ * */
+class ConstraintLayoutChainActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_constraintlayout_chain)
+    }
+}
