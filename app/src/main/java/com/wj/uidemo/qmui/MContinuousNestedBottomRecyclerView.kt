3975a5b768ac44acd21f63eecea9a64b2b801e9c
王健 <wangjian@igetcool.com>
2021-09-03 17:28:34 +0800

    代码格式化。

--- app/src/main/java/com/wj/uidemo/qmui/MContinuousNestedBottomRecyclerView.kt
+++ app/src/main/java/com/wj/uidemo/qmui/MContinuousNestedBottomRecyclerView.kt
@@ -8,5 +8,4 @@ import com.qmuiteam.qmui.nestedScroll.QMUIContinuousNestedBottomRecyclerView
  * Des 继承QMUIContinuousNestedBottomRecyclerView，可添加自定义实现
  * @author WangJian on 2021/9/3 10
  * */
-class MContinuousNestedBottomRecyclerView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyle: Int = 0) : QMUIContinuousNestedBottomRecyclerView(context, attrs, defStyle), BottomWidgetItemView {
-}
+class MContinuousNestedBottomRecyclerView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyle: Int = 0) : QMUIContinuousNestedBottomRecyclerView(context, attrs, defStyle), BottomWidgetItemView
